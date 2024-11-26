58d4217def7540f0556f3a90646097b93530a8de
az-airtech <arnoud@airtechnology.io>
2024-11-26 16:46:06 +0700

    Removed `sudo` from make and changed to install_sw target

--- COREOGRAPH.sh
+++ COREOGRAPH.sh
@@ -1,3 +1,3 @@
-sudo rm -rf openssl-install && mkdir openssl-install 
-./config -fPIC shared --prefix=`pwd`/openssl-install/ --openssldir=/etc/ssl
-make -j$(nproc); sudo make install
+rm -rf openssl-install && mkdir openssl-install 
+./config -fPIC shared --prefix=`pwd`/openssl-install/ --openssldir=`pwd`/openssl-install/
+make -j$(nproc); make install_sw
